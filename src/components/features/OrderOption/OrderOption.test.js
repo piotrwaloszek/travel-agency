23bcaf9ee125e9a409b3eebc83d4eea1b51ae082
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-05-14 13:53:12 +0200

    Add for in orderoption.test

--- src/components/features/OrderOption/OrderOption.test.js
+++ src/components/features/OrderOption/OrderOption.test.js
@@ -7,14 +7,44 @@ describe('Component OrderOption', () => {
     const component = shallow(<OrderOption type='type' name='name'/>);
     expect(component).toBeTruthy();
   });
+
   it('should return empty object if called without required props', () => {
     const component = shallow(<OrderOption />);
     expect(component).toEqual({});
   });
+
   it('should render correct component title', () => {
     const expectedName = 'name';
     const expectedType = 'text';
     const component = shallow(<OrderOption name={expectedName} type={expectedType} />);
     expect(component.find('.title').text()).toEqual(expectedName);
   });
+
+  const optionTypes = {
+    dropdown: 'OrderOptionDropdown',
+    icons: 'OrderOptionIcons',
+    checkboxes: 'OrderOptionCheckboxes',
+    number: 'OrderOptionNumber',
+    text: 'OrderOptionText',
+    date: 'OrderOptionDate',
+  };
+
+  for(let type in optionTypes){
+    describe(`Component OrderOption with type=${type}`, () => {
+      /* test setup */
+
+      /* common tests */
+      it('passes dummy test', () => {
+        expect(1).toBe(1);
+      });
+
+      /* type-specific tests */
+      switch (type) {
+        case 'dropdown': {
+          /* tests for dropdown */
+          break;
+        }
+      }
+    });
+  }
 });
